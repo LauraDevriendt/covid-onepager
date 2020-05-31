221a5a07db31dad6fdce4f3a6c7b5a5f2e5a115a
LauraDevriendt <laura_devriendt5@hotmail.com>
2020-05-31 23:45:23 +0200

    till blog

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,3 +8,30 @@ $(document).ready(function () {
   });
 
 
+  // dropdown menu's
+var dropdownButtons = document.querySelectorAll(".dropdownButton");
+
+
+
+dropdownButtons.forEach(button => {
+  button.addEventListener("click", function() {
+  
+    if(button.nextElementSibling.classList.contains('hide')==true){
+      console.log('yes');
+      button.firstElementChild.innerHTML=`<i class="fas fa-chevron-down"></i>`;
+      let sibling =button.nextElementSibling;
+    sibling.classList.remove('hide');
+   
+    }else{
+
+    button.firstElementChild.innerHTML=`<i class="fas fa-chevron-right"></i>`;
+    let sibling =button.nextElementSibling;
+    sibling.classList.add('hide');
+  
+    }
+  });
+  
+});
+   
+   
+ 
